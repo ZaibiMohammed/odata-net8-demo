d01829433ed77b81e3bd08b1a25e529f9e45bf9f
ZaibiMed <mohammed.zaibi@outlook.com>
2024-12-01 01:50:26 +0100

    Add base entity class

---
+++ src/OData.Demo.Core/Common/BaseEntity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using OData.Demo.Core.DomainEvents;
+
+namespace OData.Demo.Core.Common
+{
+    public abstract class BaseEntity
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void RemoveDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvents.Remove(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+}
